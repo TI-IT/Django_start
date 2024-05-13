4045986a8f4e2d63362b6ecd220366bffeacc6a5
TI-IT <go.tiit.ogle@email.com>
2024-05-13 11:49:29 +0300

    api_v1_01

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from tastypie.resources import ModelResource
+from shop.models import Category, Course
 
-# Create your models here.
+
+class CategoryResource(ModelResource):
+    class Meta:
+        queryset = Category.objects.all()
+        resource_name = 'categories'
+        # Разрешаем только меьод GET
+        allowed_methods = ['get']
+
+
+class CourseResource(ModelResource):
+    class Meta:
+        queryset = Course.objects.all()
+        resource_name = 'courses'
+        # Разрешаем только меьод GET
+        allowed_methods = ['get', 'post', 'delete']
